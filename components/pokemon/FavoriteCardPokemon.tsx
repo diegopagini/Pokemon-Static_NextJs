49311d53a5195c84e3eb44c284df7ee85e0be4b3
Diego Pagini <diego.pagini@neoris.com>
2022-05-24 13:08:29 +0200

    navigation

--- components/pokemon/FavoriteCardPokemon.tsx
+++ components/pokemon/FavoriteCardPokemon.tsx
@@ -1,17 +1,24 @@
 /** @format */
 import { Card } from '@nextui-org/react';
+import { useRouter } from 'next/router';
 import React, { FC } from 'react';
 
 import { environment } from '../../environment/environment';
-
 
 interface Props {
 	id: number;
 }
 
 export const FavoriteCardPokemon: FC<Props> = ({ id }) => {
+	const router = useRouter();
+
+	const onFavouriteClicked = (): void => {
+		router.push(`/pokemon/${id}`);
+	};
+
 	return (
 		<Card
+			onClick={onFavouriteClicked}
 			hoverable
 			clickable
 			css={{
