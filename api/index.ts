21d2949a2b0921158b0a3e4ed8d26a600dfb4807
Diego Pagini <diego.pagini@neoris.com>
2022-05-24 08:47:34 +0200

    pokemon

--- api/index.ts
+++ api/index.ts
@@ -1,8 +1,4 @@
 /** @format */
-import pokeFull from './pokeFull';
-
-
-/** @format */
 
 // De esta manera decimos, la exportación por defecto, tratala como pokeApi.
 export { default as pokeApi } from './pokeApi';
--- pages/pokemon/[id].tsx
+++ pages/pokemon/[id].tsx
@@ -1,4 +1,5 @@
 /** @format */
+import { Button, Card, Container, Grid, Image, Text } from '@nextui-org/react';
 import { GetStaticPaths, GetStaticProps, NextPage } from 'next';
 import { FC } from 'react';
 
@@ -7,13 +8,77 @@ import { Layout } from '../../components/layouts';
 import { Pokemon } from '../../interfaces';
 
 interface Props {
-	pokemon: any;
+	pokemon: Pokemon;
 }
 
 export const PokemonPage: NextPage<Props> = ({ pokemon }) => {
+	console.log(pokemon);
+
 	return (
-		<Layout title='asd'>
-			<h1>Temporal</h1>
+		<Layout title={pokemon.name}>
+			<Grid.Container css={{ marginTop: '5px' }} gap={2}>
+				<Grid xs={12} sm={4}>
+					<Card hoverable css={{ padding: '30px' }}>
+						<Card.Body>
+							<Card.Image
+								src={
+									pokemon.sprites.other?.dream_world.front_default ||
+									'/no-image.png'
+								}
+								alt={pokemon.name}
+								width='100%'
+								height={200}
+							/>
+						</Card.Body>
+					</Card>
+				</Grid>
+				<Grid xs={12} sm={8}>
+					<Card>
+						<Card.Header
+							css={{ display: 'flex', justifyContent: 'space-between' }}
+						>
+							<Text h1 transform='capitalize'>
+								{pokemon.name}
+							</Text>
+							<Button color='gradient' ghost>
+								Guardar en favoritos
+							</Button>
+						</Card.Header>
+						<Card.Body>
+							<Text size={30}>Sprites:</Text>
+							<Container display='flex' direction='row'>
+								<Image
+									src={pokemon.sprites.front_default}
+									alt={pokemon.name}
+									width={100}
+									height={100}
+								/>
+
+								<Image
+									src={pokemon.sprites.back_default}
+									alt={pokemon.name}
+									width={100}
+									height={100}
+								/>
+
+								<Image
+									src={pokemon.sprites.front_shiny}
+									alt={pokemon.name}
+									width={100}
+									height={100}
+								/>
+
+								<Image
+									src={pokemon.sprites.back_shiny}
+									alt={pokemon.name}
+									width={100}
+									height={100}
+								/>
+							</Container>
+						</Card.Body>
+					</Card>
+				</Grid>
+			</Grid.Container>
 		</Layout>
 	);
 };
@@ -36,13 +101,11 @@ export const getStaticPaths: GetStaticPaths = async (ctx) => {
  */
 export const getStaticProps: GetStaticProps = async ({ params }) => {
 	const { id } = params as { id: string };
-	const data = await pokeFull<Pokemon>(`/pokemon/${id}`);
-
-	console.log(data);
+	const pokemon = await pokeFull<Pokemon>(`/pokemon/${id}`);
 
 	return {
 		props: {
-			data,
+			pokemon,
 		},
 	};
 };
